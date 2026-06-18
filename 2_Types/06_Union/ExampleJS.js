165ad4c075142a31e8e4ba247b10af498370016a
SATHISH-WEBDEVBER <sathishms1589@gmail.com>
2026-06-18 22:40:43 +0530

    Add Union Type Example in JavaScript

---
+++ 2_Types/06_Union/ExampleJS.js
@@ -0,0 +1,14 @@
+// Union Type Example - JavaScript
+
+// JavaScript does not provide union types.
+// JavaScript-ல் union type என்ற கருத்து இல்லை.
+
+// A variable can hold any datatype.
+// ஒரு variable எந்த datatype-யையும் வைத்திருக்கலாம்.
+let userId = 101;
+
+console.log(userId);
+
+userId = "EMP101";
+
+console.log(userId);
