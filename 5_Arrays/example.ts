356f018af42e95f5238c6dc2d59350d2dab9add6
SATHISH-WEBDEVBER <sathishms1589@gmail.com>
2026-06-22 16:13:51 +0530

    Remove example files for classes, inheritance, closures, static properties, and enums

--- 5_Arrays/example.ts
+++
@@ -1,7 +0,0 @@
-var number : number[] = [1, 2, 3, 4, 5];
-var string : string[] = ["Hello", "World"];
-var boolean : boolean[] = [true, false, true];
-
-console.log(number);
-console.log(string);
-console.log(boolean);
--- 6_Any/Example.ts
+++
@@ -1,8 +0,0 @@
-var item1:any = {
-    id:1,
-    name:"Item 1",
-}
-
-item1 ={
-    id:2
-}
