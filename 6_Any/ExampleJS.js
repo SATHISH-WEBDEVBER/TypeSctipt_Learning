5b83a0b8ea875c957ba0f870ad7b0671a9851227
SATHISH-WEBDEVBER <sathishms1589@gmail.com>
2026-06-30 16:00:51 +0530

    Commit 1/7: Create folder 6_Any and add ExampleJS.js base

---
+++ 6_Any/ExampleJS.js
@@ -0,0 +1,11 @@
+// JavaScript operates dynamically, allowing variables to hold any type of value.
+// JavaScript மாறும் தன்மையுடன் செயல்படுவதால், variables எந்தவொரு மதிப்பையும் பெற அனுமதிக்கிறது.
+var wildcard = 100;
+console.log("Value:", wildcard);
+// Expected Output: Value: 100
+// எதிர்பார்க்கப்படும் வெளியீடு: Value: 100
+
+wildcard = "Hello";
+console.log("Updated Value:", wildcard);
+// Expected Output: Updated Value: Hello
+// எதிர்பார்க்கப்படும் வெளியீடு: Updated Value: Hello
