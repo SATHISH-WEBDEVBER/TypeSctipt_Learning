da6030770bc6a5340a8262dec50b403c9a56ad31
SATHISH-WEBDEVBER <sathishms1589@gmail.com>
2026-07-07 00:23:40 +0530

    Commit 2/7: Update ExampleJS.js with ES6 defaults

--- 10_Default_Parameters/ExampleJS.js
+++ 10_Default_Parameters/ExampleJS.js
@@ -8,3 +8,13 @@ function orderCoffee(size, milkType) {
 console.log("Order 1:", orderCoffee("Large", "Oat Milk"));
 // Expected Output: Order 1: Large coffee with Oat Milk
 // எதிர்பார்க்கப்படும் வெளியீடு: Order 1: Large coffee with Oat Milk
+
+// ES6 introduced native default parameter syntax in JavaScript.
+// ES6-ல் JavaScript-ல் நேரடி இயல்புநிலை அளபுரு (default parameter) முறை அறிமுகப்படுத்தப்பட்டது.
+function orderCoffeeES6(size, milkType = "Regular Milk") {
+    return size + " coffee with " + milkType;
+}
+
+console.log("Order 2:", orderCoffeeES6("Medium"));
+// Expected Output: Order 2: Medium coffee with Regular Milk
+// எதிர்பார்க்கப்படும் வெளியீடு: Order 2: Medium coffee with Regular Milk
